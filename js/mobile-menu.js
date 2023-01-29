167db87b5639f3b980c76081c8af00383a8472fd
Kristuwa <72200499+Kristuwa@users.noreply.github.com>
2023-01-29 23:35:20 +0200

    fix:add close on backdrop

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -3,23 +3,39 @@
   const mobileMenuRef = document.querySelector("[data-menu]");
   const linkBtn = document.querySelector(".site-nav");
 
-  const close = () => {
+  const toggleMenu = () => {
     mobileMenuRef.classList.toggle("is-open");
     menuBtnRef.classList.toggle("is-open");
+    document.body.classList.toggle("is-open");
   };
 
   linkBtn.addEventListener("click", (e) => {
     if (e.target.nodeName !== "A") {
       return;
     }
-    close();
+    toggleMenu();
   });
 
-  menuBtnRef.addEventListener("click", () => {
+  function onClickBackdrop(e) {
+    const target = e.target;
+    const its_menu = target === mobileMenuRef || mobileMenuRef.contains(target);
+    const its_btnMenu = target === menuBtnRef;
+    const menu_is_active = mobileMenuRef.classList.contains("is-open");
+
+    if (!its_menu && !its_btnMenu && menu_is_active) {
+      toggleMenu();
+    }
+  }
+
+  document.addEventListener("click", onClickBackdrop);
+
+  menuBtnRef.addEventListener("click", (e) => {
+    e.stopPropagation();
     const expanded =
       menuBtnRef.getAttribute("aria-expanded") === "true" || false;
 
     menuBtnRef.setAttribute("aria-expanded", !expanded);
-    close();
+
+    toggleMenu();
   });
 })();
