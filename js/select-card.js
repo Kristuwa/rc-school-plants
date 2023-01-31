2780295f47c849d25e8f0197b8f75a5c5c826c45
Kristuwa <72200499+Kristuwa@users.noreply.github.com>
2023-01-31 19:36:32 +0200

    fix: change function onBtnClick

--- js/select-card.js
+++ js/select-card.js
@@ -1,42 +1,82 @@
-const buttonsRef = document.querySelector(".button-container");
-const gardensRef = document.querySelectorAll("[data-garden]");
-const lawnRef = document.querySelectorAll("[data-lawn]");
-const plantingRef = document.querySelectorAll("[data-planting]");
-const buttons = document.querySelectorAll(".service__button");
+(() => {
+  const buttonsRef = document.querySelector(".button-container");
+  const serviceList = document.querySelectorAll(".service__item");
+  const buttonsList = document.querySelectorAll(".service__button");
 
-function select(array) {
-  array.forEach((item) => item.classList.toggle("active"));
-}
-const btnList = Array.from(buttons);
+  let activeListBtn = [];
+  let activeListItems = [];
 
-function onBtnClick(e) {
-  if (e.target.nodeName !== "BUTTON") {
-    return;
+  function onBtnClick(e) {
+    // Если клик не по кнопке, то выходим
+    if (e.target.nodeName !== "BUTTON") {
+      return;
+    }
+
+    //Записываем в вспомогательный массив, какую кнопку нажали
+    if (!activeListBtn.includes(e.target.dataset.name)) {
+      //если ее нет в массиве, то добавляем
+      activeListBtn.push(e.target.dataset.name);
+      //если нажато уже 2 кнопки, то класс не добавляем
+      if (activeListBtn.length <= 2) {
+        e.target.classList.add("active");
+        console.log(activeListBtn);
+        if (activeListBtn.length === 2) {
+          [...buttonsList].map((item) => {
+            if (item.classList.contains("active")) {
+              item.disabled = false;
+            } else {
+              item.disabled = true;
+            }
+          });
+        }
+      }
+      // else {
+      // 	[...buttonsList].map((item) => {
+      // 	  console.log(item.disabled);
+      // 	  if (item.classList.contains("active")) {
+      // 		 item.disabled = true;
+      // 	  }
+      // 	  item.disabled = false;
+      // 	});
+      //  }
+    }
+    // при повторном нажатии проверяем,если есть в массиве активных кнопок, то убираем класс и удаляем из массива
+    else {
+      activeListBtn = activeListBtn.filter(
+        (item) => item !== e.target.dataset.name
+      );
+      e.target.classList.remove("active");
+      if (activeListBtn.length === 1) {
+        [...buttonsList].map((item) => {
+          item.disabled = false;
+        });
+      }
+    }
+    // перебираем наши эементы галереи, и выбираем только те-которые совпадают с нажатой кнопкой
+    activeListItems = [...serviceList]
+      .map((item) => {
+        if (item.classList.contains("no-active")) {
+          item.classList.remove("no-active");
+        }
+        return item;
+      })
+      .filter((item) => {
+        return !activeListBtn.includes(item.dataset.set);
+      });
+
+    activeListItems.map((item) => {
+      return item.classList.toggle("no-active");
+    });
+
+    if (activeListItems.length === 6) {
+      activeListItems = [...serviceList].map((item) => {
+        if (item.classList.contains("no-active")) {
+          item.classList.remove("no-active");
+        }
+        return item;
+      });
+    }
   }
 
-  e.target.classList.toggle("active");
-  const btnActive = btnList.filter((item) => {
-    return item.className === "button service__button active";
-  });
-  const noActiveBtn = btnActive.find(
-    (item) =>
-      item.className !== "button service__button active" &&
-      btnActive.length === 2
-  );
-  console.log(noActiveBtn);
-  noActiveBtn.setAttribute("disabled");
-  if (e.target.textContent === "Gardens") {
-    select(plantingRef);
-    select(lawnRef);
-  }
-  if (e.target.textContent === "Lawn") {
-    select(gardensRef);
-    select(plantingRef);
-  }
-  if (e.target.textContent === "Planting") {
-    select(gardensRef);
-    select(lawnRef);
-  }
-}
-
-buttonsRef.addEventListener("click", onBtnClick);
+  buttonsRef.addEventListener("click", onBtnClick);
+})();
